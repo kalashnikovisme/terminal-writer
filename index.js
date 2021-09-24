b3f71d13c1a29add96336ff23b1456ecf321903c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2021-09-25 03:42:49 +0400

    MOre scenarios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let bashPrompt = '~:'
-let term = new Terminal({ cols: 120, rows: 40, fontSize: '20' });
+let term = new Terminal({ cols: 120, rows: 40, fontSize: '19' });
 const typingTimeout = 100
 const directives = ['input:', 'output:', 'audio:', 'delay:']
 const newLine = '\n\r'
